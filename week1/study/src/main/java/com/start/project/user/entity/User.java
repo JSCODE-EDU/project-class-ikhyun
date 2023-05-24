c099619b41734da2d2c13a9517ee12029b4c1909
DESKTOP-FV1N1HI\user <whdlrgus32@naver.com>
2023-05-25 00:20:11 +0900

    회원테이블 추가

---
+++ week1/study/src/main/java/com/start/project/user/entity/User.java
@@ -0,0 +1,29 @@
+package com.start.project.user.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Data
+@ToString
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private LocalDateTime createdAt;
+}
